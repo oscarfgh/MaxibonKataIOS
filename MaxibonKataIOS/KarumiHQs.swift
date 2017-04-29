abec16fa15b4a71d1eaac934f504e99356fa0263
Óscar García <oscargarcia@MacBook-Pro-de-Oscar.local>
2017-04-29 12:04:37 +0200

    KarumiHQs y primer test.

--- MaxibonKataIOS/KarumiHQs.swift
+++ MaxibonKataIOS/KarumiHQs.swift
@@ -1,59 +1,31 @@
 //
 //  KarumiHQs.swift
-//  MaxibonKata
+//  MaxibonKataIOS
 //
-//  Created by Pedro Vicente Gomez on 29/06/16.
-//  Copyright © 2016 GoKarumi. All rights reserved.
+//  Created by Óscar García on 29/4/17.
+//  Copyright © 2017 GoKarumi. All rights reserved.
 //
 
 import Foundation
 
-public final class KarumiHQs {
-
-    fileprivate let chat: Chat
-
-    var maxibonsLeft: Int
-
-    fileprivate var shouldBuyMoreMaxibons: Bool {
-        return maxibonsLeft <= 2
-    }
-
-    public init(chat: Chat) {
-        self.maxibonsLeft = 10
-        self.chat = chat
-    }
-
-    public convenience init() {
-        self.init(chat: Slack())
-    }
-
-    public func openFridge(_ developer: Developer) {
-        openFridge([developer])
-    }
-
-    public func openFridge(_ developers: [Developer]) {
-        developers.forEach { developer in
-            grabMaxibons(developer)
-            if shouldBuyMoreMaxibons {
-                notifyWeShouldBuyMaxibons(developer)
-                buyMoreMaxibons()
-            }
-        }
-    }
-
-    fileprivate func notifyWeShouldBuyMaxibons(_ developer: Developer) {
-        let message = "Hi guys, I'm \(developer). We need more maxibons!"
-        chat.send(message: message)
-    }
-
-    fileprivate func buyMoreMaxibons() {
-        maxibonsLeft += 10
-    }
-
-    fileprivate func grabMaxibons(_ developer: Developer) {
-        maxibonsLeft -= developer.numberOfMaxibonsToGet
+final public class KarumiHQs {
+    var maxibonsLeft = 10
+    
+    func openFridge(developer : Developer) {
+        maxibonsLeft -= developer.numberOfMaxibonsToGrap
+        
         if maxibonsLeft < 0 {
             maxibonsLeft = 0
+        }
+        
+        if maxibonsLeft <= 2 {
+           maxibonsLeft += 10
+        }
+    }
+    
+    func openFridge(developers : [Developer]) {
+        for developer in developers {
+            openFridge(developer: developer)
         }
     }
 }
